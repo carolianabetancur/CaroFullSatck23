c6b388ccbce710fd504846a73343c016a2263030
Carolina Perez Betancur <carolinabetancur24@hotmail.com>
2026-07-23 18:48:19 -0500

    feat: Add javascript and typescript examples

---
+++ typescript/index.js
@@ -0,0 +1,44 @@
+"use strict";
+console.log('Hello World');
+class PersonaTsc {
+    //Estos datos pueden estar private, protected, public; private nombre: string
+    nombre;
+    edad;
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    //Las funciones y los métodos su tipado es :void
+    saludar() {
+        console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad} años`);
+    }
+}
+const personaTsc1 = new PersonaTsc('David', 34);
+console.log(personaTsc1);
+personaTsc1.saludar();
+const perro = {
+    nombre: 'Firulais',
+    edad: 5,
+    raza: 'Pitbull',
+    saludar() {
+        console.log(`Guau, guau, me llamo ${this.nombre}`);
+    },
+};
+console.log(perro);
+perro.saludar();
+const producto1 = {
+    id: 1,
+    nombre: 'Portátil',
+    precio: 900,
+    stock: 5,
+};
+console.log(producto1);
+//Enumeradores (enum): Es para tener variables predefinidas, solo voy a poder utilizar lo que esté aquí
+var Colores;
+(function (Colores) {
+    Colores["ROJO"] = "ROJO";
+    Colores["VERDE"] = "VERDE";
+    Colores["AZUL"] = "AZUL";
+})(Colores || (Colores = {}));
+const colorFavorito = Colores.ROJO;
+console.log(colorFavorito);
---
+++ typescript/index.ts
@@ -0,0 +1,66 @@
+console.log('Hello World');
+
+class PersonaTsc {
+  //Estos datos pueden estar private, protected, public; private nombre: string
+  nombre: string;
+  edad: number;
+  constructor(nombre: string, edad: number) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+  //Las funciones y los métodos su tipado es :void
+  saludar(): void {
+    console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad} años`);
+  }
+}
+
+const personaTsc1 = new PersonaTsc('David', 34);
+console.log(personaTsc1);
+personaTsc1.saludar();
+
+//Interfaces: se pueden asimilar a la forma en la que se crean objetos a partir de funciones, son estructuras que nos permiten
+//el cuerpo de la función o declara las funciones sin darle un cuerpo o una interpretación completa. Son moldes para reutilozar
+interface Animal {
+  nombre: string;
+  edad: number;
+  raza: string;
+  saludar(): void;
+}
+
+const perro: Animal = {
+  nombre: 'Firulais',
+  edad: 5,
+  raza: 'Pitbull',
+  saludar() {
+    console.log(`Guau, guau, me llamo ${this.nombre}`);
+  },
+};
+console.log(perro);
+perro.saludar();
+
+//También se pueden declarar objetos con una estructura mas sencilla llamada type
+type Producto = {
+  id: number;
+  nombre: string;
+  precio: number;
+  stock: number;
+};
+
+const producto1: Producto = {
+  id: 1,
+  nombre: 'Portátil',
+  precio: 900,
+  stock: 5,
+};
+console.log(producto1);
+
+//Enumeradores (enum): Es para tener variables predefinidas, solo voy a poder utilizar lo que esté aquí
+
+enum Colores {
+  ROJO = 'ROJO',
+  VERDE = 'VERDE',
+  AZUL = 'AZUL',
+}
+
+const colorFavorito: Colores = Colores.ROJO;
+console.log(colorFavorito);
